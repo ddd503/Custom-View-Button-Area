fe80f4d8cc17e62327aa8180208b692302a988ce
ddd503 <skbsporta7@gmail.com>
2019-07-17 23:33:29 +0900

    add base layout

---
+++ Custom-View-Button-Area/ButtonAreaView/ButtonAreaView.swift
@@ -0,0 +1,17 @@
+//
+//  ButtonAreaView.swift
+//  Custom-View-Button-Area
+//
+//  Created by kawaharadai on 2019/07/17.
+//  Copyright © 2019 kawaharadai. All rights reserved.
+//
+
+import UIKit
+
+final class CustomButtonAreaView: UIView {
+
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+
+}
