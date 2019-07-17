83d55d28c25b1490e7c9936f513814048c727649
ddd503 <skbsporta7@gmail.com>
2019-07-17 23:12:31 +0900

    Initial commit

---
+++ Custom-View-Button-Area/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Custom-View-Button-Area
+//
+//  Created by kawaharadai on 2019/07/17.
+//  Copyright © 2019 kawaharadai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
